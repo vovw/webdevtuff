930073a476b97061d4a58a74812c2c692ab915f8
unknown <harshal011kamble@gmail.com>
2023-11-16 20:12:19 +0530

    just minor changes

--- src/App.js
+++ src/App.js
@@ -3,23 +3,18 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Timeline from "./components/timeline/timeline";
 import Home from './components/Home/home';
 import Events from './components/Events/events';
-import Navbar from "./components/navbar/navbar";
-import Key_high from "./components/key_highlightssec/key_high";
-import Fotter from './components/fotter/fotter';
+import Navbar from "./components/navbar/navbar"
 import Gls from './components/gls/gls'
 import eventsData from './components/Events/data';
 import EventInfo from './components/Events/EventInfo';
 import Team from './components/team/team'
 import TandC from './components/rules/terms';
 import Privacy from './components/rules/privacy';
-// not sure how to fix sponsorship maybe we need to add another navbar for this gib tips
-// import Appp from './components/sponsorships/spons'
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      
       <Routes>
         <Route exact path="/" element={<Home />} />
         <Route exact path='/gallery' element={<Timeline />} />
@@ -31,7 +26,6 @@ function App() {
         {eventsData.map((e) => <Route exact path={'/events/' + e.title} element={<EventInfo title={e.title} imgname={e.imgname} eventDesc={e.description} venue={e.venue} date={e.date} price={e.price} coordinator1={e.coordinator1} coordinator2={e.coordinator2} contact1={e.contact1} contact2={e.contact2} />} />)}
         <Route />
       </Routes>
-      <Key_high/>
     </BrowserRouter>
   );
 }
--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,17 +1,22 @@
 import "./home.css";
-
+import Key_high from "../key_highlightssec/key_high"
+import Fotter from "../fotter/fotter";
 export default function Navbar() {
   return (
+    <>
     <section className="landing h-screen text-white bg-black flex items-center justify-start w-screen">
-  <div className="title-container h-1/2 mx-16">
-    <h1 className="text-8xl" data-aos="fade-down">
-      <span className="txgradient">Technovanza</span>
-    </h1>
-    <h2 className="text-4xl" data-aos="fade-down">
-      Warp into a Far Future
-    </h2>
-  </div>
-</section>
+      <div className="title-container h-1/2 mx-16">
+        <h1 className="text-8xl" data-aos="fade-down">
+          <span className="txgradient">Technovanza</span>
+        </h1>
+        <h2 className="text-4xl" data-aos="fade-down">
+          Warp into a Far Future
+        </h2>
+      </div>
 
+    </section>
+    <Key_high />
+    <Fotter position="static" />
+</>
   );
 }
