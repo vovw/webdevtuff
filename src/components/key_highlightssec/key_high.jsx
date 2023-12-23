d68483c297cc41fa6a95116bf215007c6c457cd0
atharva <voidz7447@gmail.com>
2023-12-23 20:05:32 +0530

    fixed bullshishhshs

--- src/components/key_highlightssec/key_high.jsx
+++ src/components/key_highlightssec/key_high.jsx
@@ -1,5 +1,5 @@
 import "./key_high.css";
-import banner from './banner.jpg';
+import banner from './PSS00072.JPG';
 
 export default function Key_high() {
   return (
@@ -19,12 +19,6 @@ export default function Key_high() {
           <div className="container px-5 pb-24 mx-auto flex flex-wrap">
             <div
               className="lg:w-1/2 w-full mb-10 lg:mb-0 rounded-lg overflow-hidden"
-              // data-tilt=""
-              // data-tilt-max={25}
-              // data-tilt-speed={100}
-              // data-tilt-perspective={20000}
-              // data-tilt-glare=""
-              // data-tilt-max-glare="0.5"
             >
               <img
                 alt="feature"
@@ -36,19 +30,6 @@ export default function Key_high() {
             </div>
             <div className="flex flex-col flex-wrap justify-center lg:py-6 -mb-10 lg:w-1/2 lg:pl-12 lg:text-left text-center">
               <div className="flex flex-col mb-10 lg:items-start items-center">
-                <div className="w-12 h-12 inline-flex items-center justify-center rounded-full bg-gray-800 text-purple-400 mb-5">
-                  <svg
-                    fill="none"
-                    stroke="currentColor"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    className="w-6 h-6"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M22 12h-4l-3 9L9 3l-3 9H2" />
-                  </svg>
-                </div>
                 <div className="flex-grow">
                   <h2 className="text-white text-4xl title-font font-medium mb-3">
 					  Technovanza
@@ -56,21 +37,7 @@ export default function Key_high() {
                   <p className="leading-relaxed des-key text-lg">
 	  Technovanza is the annual technical fest of VJTI, where students strive for excellence while giving back to the community. Over the last 18 years, it has grown to become one of the most popular Techno-managerial events in the country and continues to awe its audience with the quality of events it hosts : From conducting competitive events, where people all over India can showcase their talents to hosting exhibitions and dignitaries from various fields, Technovanza keeps getting better each year. Keeping up with our motto of “Taking Technology to the society”, we continue to deliver an exuberant event, virtually this year!
                   </p>
-                  <a className="mt-3 text-purple-400 inline-flex items-center">
-                    RSVP
-                    <svg
-                      fill="none"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={2}
-                      className="w-4 h-4 ml-2"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M5 12h14M12 5l7 7-7 7" />
-                    </svg>
-                  </a>
-                </div>
+                  </div>
               </div>
             </div>
           </div>
