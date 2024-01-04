2b9dbcfb710396c2e745fe4a8307643c8badc5bc
HimajPatil <78681144+HimajPatil@users.noreply.github.com>
2024-01-05 00:02:43 +0530

    fix fix fix

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -1,28 +1,34 @@
 import "./home.css";
-import Key_high from "../key_highlightssec/key_high"
+import Key_high from "../key_highlightssec/key_high";
 import Fotter from "../fotter/fotter";
 export default function Navbar() {
   return (
     <>
-    <section className="landing h-screen text-white bg-black flex items-center justify-start w-screen">
- <div className="title-container h-1/2 mx-16">
-   <h1 className="text-8xl" data-aos="fade-down">
-     Technovanza
-   </h1>
-   <h2 className="text-4xl" data-aos="fade-down">
-     Warp drive into the Far Future
-   </h2>
- </div>
- <div className="button-container">
-  <a href="/sponsorships">
-   <button className="btn-gradient btn-gradient--color__1">Sponsors</button>
-   </a>
-   <a href="/Hospitality"><button className="btn-gradient btn-gradient--color__2">Hospitality</button></a>
- </div>
-</section>
+      <section className="landing h-screen text-white bg-black flex items-center justify-start w-screen">
+        <div className="title-container h-1/2 mx-16">
+          <h1 className="text-8xl" data-aos="fade-down">
+            Technovanza
+          </h1>
+          <h2 className="text-4xl" data-aos="fade-down">
+            Warp drive into the Far Future
+          </h2>
+        </div>
+        <div className="button-container">
+          <a href="/sponsorships">
+            <button className="btn-gradient btn-gradient--color__1">
+              Sponsors
+            </button>
+          </a>
+          <a href="/Hospitality">
+            <button className="btn-gradient btn-gradient--color__2">
+              Hospitality
+            </button>
+          </a>
+        </div>
+      </section>
 
-    <Key_high />
-    <Fotter position="static" />
-</>
+      <Key_high />
+      <Fotter position="static" />
+    </>
   );
-  }
+}
