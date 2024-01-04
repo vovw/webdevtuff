2307b0a0c5500db4b6ee3434ea48e8d94e2c2b1d
HimajPatil <78681144+HimajPatil@users.noreply.github.com>
2024-01-05 00:00:53 +0530

    font fix3

--- src/components/Home/home.jsx
+++ src/components/Home/home.jsx
@@ -7,7 +7,7 @@ export default function Navbar() {
     <section className="landing h-screen text-white bg-black flex items-center justify-start w-screen">
  <div className="title-container h-1/2 mx-16">
    <h1 className="text-8xl" data-aos="fade-down">
-     <span className="txgradient">Technovanza</span>
+     Technovanza
    </h1>
    <h2 className="text-4xl" data-aos="fade-down">
      Warp drive into the Far Future
