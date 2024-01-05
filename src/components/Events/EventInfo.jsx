eb8a75cb58739661d34b590045de96fb2d857f2a
devXyz <ranesoham301@gmail.com>
2024-01-05 14:57:18 +0530

    Added event coordinators

--- src/components/Events/EventInfo.jsx
+++ src/components/Events/EventInfo.jsx
@@ -3,7 +3,7 @@ import Accordion from './faq';
 import './EventInfo.css';
 const bgImage = require('../../img/deep-space-bg.jpg');
 
-export default function EventInfo ({ title, imgname, eventDesc, link, docum}){
+export default function EventInfo ({ title, imgname, eventDesc, link, docum,coordinator1,coordinator2,contact1,contact2}){
     return(
         <div className='eventsinfo'>
             <img className='event-bgimg' src={bgImage} alt=''/>
@@ -29,6 +29,22 @@ export default function EventInfo ({ title, imgname, eventDesc, link, docum}){
                     Rule Book
                  </button>
                 </a>
+                </div>
+                <div className="coordinators-section">
+                    <div className="coordinator-label">Event Coordinators</div>
+                    <div className="coordinator-container">
+                        <div className="coordinator">
+                            <div className="coordinator-name">{coordinator1}</div>
+                            <div className="coordinator-phone">{contact1}</div>
+                        </div>
+                    </div>
+                    <div className="coordinator-container">
+                        <div className="coordinator">
+                            <div className="coordinator-name">{coordinator2}</div>
+                            <div className="coordinator-phone">{contact2}</div>
+                        </div>
+                    </div>
+                    {/* Add more coordinators as needed */}
                 </div>
 
                 <div style={{backgroundColor: 'black'}}>
--- src/components/Events/data.js
+++ src/components/Events/data.js
@@ -6,7 +6,11 @@ let eventsData = [
         brief: "VRC stands for VJTI Robotics Challenge, one of the very popular and respected competitions conducted...",
         description: "Embark on a thrilling cosmic odyssey in 'Trailblazing the Cosmos,' as manual and auto bots navigate challenges, gather resources, and unravel the mysteries of the universe, culminating in a symbol of hope for humanity at VRC 11.0.",
         reglink: "https://unstop.com/p/vrc-technovanza-vjti-veermata-jijabai-technological-institute-vjti-mumbai-856134",
-        pdf:"https://unstop.com/api/competition/get-attachment/6593ade1564de_VRC_merged.pdf"
+        pdf:"https://unstop.com/api/competition/get-attachment/6593ade1564de_VRC_merged.pdf",
+        coordinator1: 'Swastik',
+        coordinator2: 'Nivedita',
+        contact1: '7506771667',
+        contact2: '7039254058',
     },
     {
         id: 1,
@@ -14,7 +18,11 @@ let eventsData = [
         imgname: "smartcity.png",
         brief: "Technovanza presents you an opportunity to put forward your thoughts on creating a smarter future fo...",
         description: "Technovanza presents you an opportunity to put forward your thoughts on creating a smarter future for the world through this event. The Smart City agenda entails improving the citizens' quality of life, strengthening and diversifying the economy while prioritizing environmental sustainability through adoption of smart solutions.",
-        reglink: "https://docs.google.com/forms/d/e/1FAIpQLSdKZjRpNVQKWc9bik8hQYOqrqg0GXKzwJxinnx_C-rc_RFdNA/viewform"
+        reglink: "https://docs.google.com/forms/d/e/1FAIpQLSdKZjRpNVQKWc9bik8hQYOqrqg0GXKzwJxinnx_C-rc_RFdNA/viewform",
+        coordinator1: 'Tejas',
+        coordinator2: 'Rahul',
+        contact1: '7719864127',
+        contact2: '9021258686',
     },
     {
         id: 2,
@@ -23,7 +31,11 @@ let eventsData = [
         brief: "VJTI Technovanza presents to you Aqua. Aqua is a waterborne event. The theme for this year is buildi...",
         description: "VJTI Technovanza presents to you Aqua. Aqua is a waterborne event. The theme for this year is building a dyson sphere around the sun.",
         reglink: "https://unstop.com/competitions/aqua-technovanza-vjti-veermata-jijabai-technological-institute-vjti-mumbai-853273",
-        pdf: "https://unstop.com/api/competition/get-attachment/658dc103978d7_AQUA_-1_merged.pdf"
+        pdf: "https://unstop.com/api/competition/get-attachment/658dc103978d7_AQUA_-1_merged.pdf",
+        coordinator1: 'Revati',
+        coordinator2: 'Sumeet',
+        contact1: '7020123750',
+        contact2: '7506180757',
     },
     {
         id: 3,
@@ -31,8 +43,11 @@ let eventsData = [
         imgname: "escaperoom.jpg",
         brief: "In the world of cosmos, you and your team now have a chance to be astronauts and explore the univers...",
         description: "In the world of cosmos, you and your team now have a chance to be astronauts and explore the universe, which has been the dream since forever. Spaceship X69420 is now recruiting people interested in becoming an astronaut and gaining this wonderful experience wherein they will learn and explore space.",
-        reglink: "https://docs.google.com/forms/d/e/1FAIpQLSeMW4UBTDdbL6n-KIWYMx5DYYwQ3-Wpdzdy0o7JFe1oJzFarg/viewform"
-        
+        reglink: "https://docs.google.com/forms/d/e/1FAIpQLSeMW4UBTDdbL6n-KIWYMx5DYYwQ3-Wpdzdy0o7JFe1oJzFarg/viewform",
+        coordinator1: 'Tulika',
+        coordinator2: 'Soham',
+        contact1: '8424016553',
+        contact2: '8007703970',
     },
     {
         id: 4,
@@ -41,7 +56,11 @@ let eventsData = [
         description: "The last few months belonged to our favourite sport, with our favourite players shining in the world cup. Now it is your turn to touch the grass with your bots. The game will have 2 bots clashing. The arena will be divided into two halves, one half dedicated to the attacker bot, and other to the defender bot, each match will consist of two halves.",
         imgname: "robosoccer.png",
         reglink: "https://unstop.com/competitions/robosoccer-technovanza-vjti-veermata-jijabai-technological-institute-vjti-mumbai-853292",
-        pdf: "https://unstop.com/api/competition/get-attachment/658dc4e439d66_ROBOSOCCER_merged.pdf"
+        pdf: "https://unstop.com/api/competition/get-attachment/658dc4e439d66_ROBOSOCCER_merged.pdf",
+        coordinator1: 'Shruti',
+        coordinator2: '8600018391',
+        contact1: 'Swastik',
+        contact2: '7506771668',
     },
     {
         id: 5,
@@ -50,7 +69,11 @@ let eventsData = [
         description: "TECHNOVANZA 2022-23 presents to you the most eccentric and enigmatic Robo event 'Robostrike'. The gameplay represents a blend of 'Air Hockey' and 'Robotics'. RoboStrike will have 2 bots going head to head, the gameplay will consist of 3 rounds.",
         imgname: "robostrike.png",
         reglink: "https://unstop.com/events/robostrike-technovanza-vjti-veermata-jijabai-technological-institute-vjti-mumbai-853302",
-        pdf: "https://unstop.com/api/competition/get-attachment/658dc7b265952_robostrike_complete_merged.pdf"
+        pdf: "https://unstop.com/api/competition/get-attachment/658dc7b265952_robostrike_complete_merged.pdf",
+        coordinator1: 'Manas',
+        coordinator2: 'Raghav',
+        contact1: '81693224193',
+        contact2: '9370892515',
     },
     {
         id: 6,
@@ -59,7 +82,11 @@ let eventsData = [
         description: "Monster arena brings you a fierce obstacle course that will test the strength of the bot and the will of the team crossing those obstacles together.",
         imgname: "monsterarena.png",
         reglink: "https://unstop.com/events/monster-arena-technovanza-vjti-veermata-jijabai-technological-institute-vjti-mumbai-853050",
-        pdf: "https://unstop.com/api/competition/get-attachment/658d911b2c9e4_Monster_Arena_Draft_2024.pdf"
+        pdf: "https://unstop.com/api/competition/get-attachment/658d911b2c9e4_Monster_Arena_Draft_2024.pdf",
+        coordinator1: 'Rushikesh',
+        coordinator2: 'Atharva',
+        contact1: '9049398094',
+        contact2: '8999389615',
     },
     {
       id: 7,
@@ -68,7 +95,11 @@ let eventsData = [
       description: "In a bustling city, Max, a former graffiti artist turned archaeologist, seeks wealth and luxury. To achieve this, he plans an elaborate heist on the Temple Run Museum, facing challenges of a clear path and a formidable security team led by Detective Sarah. As they both prepare for the showdown, the story unfolds as an epic battle of wits, determination, and destiny.",
       imgname:"technoquaser.png",
       reglink: "https://unstop.com/p/technoescapade-technovanza-vjti-veermata-jijabai-technological-institute-vjti-mumbai-854850",
-      pdf: "https://unstop.com/api/competition/get-attachment/65905a8e5a445_escapade.pdf"
+      pdf: "https://unstop.com/api/competition/get-attachment/65905a8e5a445_escapade.pdf",
+      coordinator1: 'TBD',
+      coordinator2: 'TBD',
+      contact1: 'TBD',
+      contact2: 'TBD',
 
     },
     {
@@ -78,7 +109,11 @@ let eventsData = [
       description: "Exploring the theoretical concept of wormholes, the narrative highlights their potential for faster-than-light travel and connections between distant parts of the universe. While remaining a theoretical idea, the pursuit of uncovering the secrets of wormholes is presented as a tantalizing endeavor, inviting those with programming skills, scientific knowledge, and creativity to embark on a journey into the unknown.",
       imgname:"co_define.png",
       reglink: "https://unstop.com/hackathons/technodyssey-technovanza-vjti-veermata-jijabai-technological-institute-vjti-mumbai-854809",
-      pdf: "https://unstop.com/api/competition/get-attachment/659055551b509_TECHNODYSSEY__1_.pdf"
+      pdf: "https://unstop.com/api/competition/get-attachment/659055551b509_TECHNODYSSEY__1_.pdf",
+      coordinator1: 'TBD',
+      coordinator2: 'TBD',
+      contact1: 'TBD',
+      contact2: 'TBD',
 
     }
 
